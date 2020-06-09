87f9abe422c7a4c2a3facf398a8d15be0a1d9bf9
Nghi <luubieunghi@gmail.com>
2020-06-09 22:44:11 +0700

    Done UserMappers

---
+++ src/java/chapter3/bo/AdminBO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter3.bo;
+
+import javax.servlet.ServletContext;
+
+/**
+ *
+ * @author mirushi
+ */
+public class AdminBO {
+    
+    private ServletContext context;
+    
+    public AdminBO(ServletContext context){
+        this.context = context;
+    }
+    
+    public 
+    
+}
